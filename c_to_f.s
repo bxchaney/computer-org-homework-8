33b3ece534ba61b45174de58944e928d97a8ec99
bxchaney <bxchaney@gmail.com>
2023-07-18 20:19:51 -0400

    added implementation

--- c_to_f.s
+++ c_to_f.s
@@ -4,8 +4,33 @@ main:
     SUB sp, sp, #4
     STR lr, [sp, #0]
 
+    # prompt
+    LDR r0, =prompt
+    BL printf
 
+    # scanf
+    LDR r0, =input
+    LDR r1, =celsius
+    BL scanf
+
+    # call CToF
+    LDR r0, =celsius
+    LDR r0, [r0]
+    BL CToF
+
+    # print result
+    MOV r2, r0
+    LDR r1, =celsius
+    LDR r1, [r1]
+    LDR r0, =output
+    BL printf
     
     LDR lr, [sp]
     ADD sp, sp, #4
     MOV pc, lr
+
+.data
+    prompt: .asciz "Please provide a temperature in C: "
+    input: .asciz "%d"
+    celsius: .word 0
+    output: .asciz "%d C is %d F\n"
